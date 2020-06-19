13972ee4e076d14f5e38d7761fcf4e2d7f9b8a63
Daniel Villavicencio <62067104+x1hntr@users.noreply.github.com>
2020-06-18 23:53:54 -0500

    equals

---
+++ Functions/iguales.m
@@ -0,0 +1,11 @@
+function iguales(A, B, C)
+% FUNCION PARA DETERMINAR: Si 3 matrices son iguales
+% Se debe ingresar 3 matrices para comprobar
+if A==B
+    if B==A
+       disp('Matrices Iguales')
+    end
+else 
+    disp('Matrices No Iguales')
+end
+end
